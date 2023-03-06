29ff39a675f55f99ba893b13d2ec07fee096754d
cdiproperzio1 <catherine.diproperzio@gmail.com>
2023-03-06 17:12:47 -0500

    added profile page

--- InnerPeas/Controllers/Tabs/ProfileViewController.swift
+++ InnerPeas/Controllers/Tabs/ProfileViewController.swift
@@ -1,31 +1,64 @@
-//
-//  ProfileViewController.swift
-//  InnerPeas
-//
-//  Created by Justin Hamilton on 2/13/23.
-//
-
 import UIKit
+import Firebase
+import FirebaseAuth
 
-class ProfileViewController: UIViewController {
-
+class ProfileViewController: UIViewController, UICollectionViewDelegate  {
+    
+    // private var collectionView: UICollectionView? = nil
+    
+    let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .green
+        return imageView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
         view.backgroundColor = .systemBackground
-
-        print("this is a test")
+        view.addSubview(profileImageView)
+        view.addSubview(containerView)
+        containerView.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 300)
+        //configureCollectionView()
+        profileImageView.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 44, paddingLeft: 32, width: 120, height: 120)
+        
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UIView {
+    //reusable function to add constraints
+    func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat? = 0, paddingLeft: CGFloat? = 0, paddingBottom: CGFloat? = 0, paddingRight: CGFloat? = 0, width: CGFloat? = nil, height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top{
+            topAnchor.constraint(equalTo: top, constant:paddingTop!).isActive = true
+        }
+        
+        if let left = left{
+            leftAnchor.constraint(equalTo: left, constant:paddingLeft!).isActive = true
+        }
+        
+        if let right = right{
+            if let paddingRight = paddingRight {
+                self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+            }
+        }
+        if let bottom = bottom{
+            if let paddingBottom = paddingBottom {
+                self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+            }
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if let height = height{
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
     }
-    */
-
 }
