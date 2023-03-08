9d29826a52647ab8831857157162ce714347229b
apotter3 <67129955+apotter3@users.noreply.github.com>
2023-03-08 00:57:25 -0500

    Updated profile with collection view(Build on another computer...)

--- InnerPeas/Controllers/Tabs/ProfileViewController.swift
+++ InnerPeas/Controllers/Tabs/ProfileViewController.swift
@@ -4,11 +4,12 @@ import FirebaseAuth
 
 class ProfileViewController: UIViewController, UICollectionViewDelegate  {
     
-    // private var collectionView: UICollectionView? = nil
+    private var collectionView: UICollectionView? = nil
+    private var viewModels = [[HomeFeedCellType]()]
     
     let containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .blue
+        view.backgroundColor = .systemGreen
         return view
     }()
     let profileImageView: UIImageView = {
@@ -26,6 +27,44 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate  {
         //configureCollectionView()
         profileImageView.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 44, paddingLeft: 32, width: 120, height: 120)
         
+    }
+    
+    private func fetchPost(){
+        //test data
+        let postData: [HomeFeedCellType] =
+        [
+            .poster(
+                viewModel: PosterCollectionViewCell(
+                    username: "FoodThatSmacks",
+                    profilePictureURL: URL(string: "https://mymodernmet.com/wp/wp-content/uploads/2020/01/baby-yoda-eating-food-13.jpg")!
+          
+                )
+            ),
+            .post(
+                viewModel: PostCollectionViewCell(
+                    postUrl: URL(string: "https://www.foodandwine.com/thmb/Yt46CaGExGFVjruxJsNSFjNVMo0=/2000x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Super-Scoops-FT-2-MAG0622-00568f6534a44e0c8a422b66b25d6cf6.jpg")!
+                )
+            ),
+            
+            .postDescription(
+                viewModel: PostDescriptionCollectionViewCell(
+                    name: "Ice Cream with a long title to see how it holds in the frame",
+                    isMade: false,
+                    isFav: false
+                )
+            ),
+            
+            .postRating(
+                viewModel: PostRatingCollectionViewCell(
+                    averageRating: 3
+                )
+            ),
+            
+            .thumbNails(viewModel: ThumbnailsCollectionViewCell())
+            
+        ]
+        viewModels.append(postData)
+        collectionView?.reloadData()
     }
 }
 
