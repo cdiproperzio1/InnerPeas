0a3ac5ab050f0af2f4deb08b7d182fd091b87986
apotter3 <67129955+apotter3@users.noreply.github.com>
2023-03-18 13:05:43 -0400

    Fixed errors with extension by repositioning closing brackets. Profile image is stretched too wide.

--- InnerPeas/Controllers/Tabs/ProfileViewController.swift
+++ InnerPeas/Controllers/Tabs/ProfileViewController.swift
@@ -5,7 +5,7 @@ import FirebaseAuth
 class ProfileViewController: UIViewController, UICollectionViewDelegate  {
     
     private var collectionView: UICollectionView? = nil
-    
+    private var viewModels = [[HomeFeedCellType]()]
     
     let User = Auth.auth().currentUser
     
@@ -23,20 +23,21 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate  {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-        
+    
     lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemGreen
         
         view.addSubview(profileImageView)
-        profileImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        profileImageView.anchor(left: view.leftAnchor, paddingLeft: 32, width: 120, height: 120)
+        profileImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImageView.anchor(top: view.topAnchor, paddingTop: 88, width: 120, height: 120)
+        //profileImageView.layer.cornerRadius = 120 / 2
         return view
     }()
     
     let profileImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = Image
+        let imageView = UIImageView ()
+        imageView.backgroundColor = .systemPink
         return imageView
     }()
     
@@ -44,33 +45,35 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate  {
         super.viewDidLoad()
         title = user.email
         view.backgroundColor = .systemBackground
-        configure()  
+        configure()
+        fetchPost()
         view.addSubview(profileImageView)
         view.addSubview(containerView)
         containerView.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, height: 300)
         //configureCollectionView()
         profileImageView.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 44, paddingLeft: 32, width: 120, height: 120)
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)        
+        super.viewDidAppear(animated)
     }
     
     private func configure() {
-    if isCurrentUser{
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            image: UIImage(systemName: "gear"),
-            style: .done,
-            target: self,
-            action: #selector(didTapSettings)
+        if isCurrentUser{
+            navigationItem.rightBarButtonItem = UIBarButtonItem(
+                image: UIImage(systemName: "gear"),
+                style: .done,
+                target: self,
+                action: #selector(didTapSettings)
             )
         }
     }
-
+    
     @objc func didTapSettings(){
         let vc = SettingsViewController()
         present(UINavigationController(rootViewController: vc), animated: true)
     }
+    
     private func fetchPost(){
         //test data
         let postData: [HomeFeedCellType] =
@@ -79,7 +82,7 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate  {
                 viewModel: PosterCollectionViewCell(
                     username: "FoodThatSmacks",
                     profilePictureURL: URL(string: "https://mymodernmet.com/wp/wp-content/uploads/2020/01/baby-yoda-eating-food-13.jpg")!
-          
+                    
                 )
             ),
             .post(
@@ -88,28 +91,28 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate  {
                 )
             ),
             
-            .postDescription(
-                viewModel: PostDescriptionCollectionViewCell(
-                    name: "Ice Cream with a long title to see how it holds in the frame",
-                    isMade: false,
-                    isFav: false
-                )
-            ),
+                .postDescription(
+                    viewModel: PostDescriptionCollectionViewCell(
+                        name: "Ice Cream with a long title to see how it holds in the frame",
+                        isMade: false,
+                        isFav: false
+                    )
+                ),
             
-            .postRating(
-                viewModel: PostRatingCollectionViewCell(
-                    averageRating: 3
-                )
-            ),
+                .postRating(
+                    viewModel: PostRatingCollectionViewCell(
+                        averageRating: 3
+                    )
+                ),
             
-            .thumbNails(viewModel: ThumbnailsCollectionViewCell())
+                .thumbNails(viewModel: ThumbnailsCollectionViewCell())
             
         ]
         viewModels.append(postData)
         collectionView?.reloadData()
     }
 }
-    
+
 extension UIView {
     //reusable function to add constraints
     func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat? = 0, paddingLeft: CGFloat? = 0, paddingBottom: CGFloat? = 0, paddingRight: CGFloat? = 0, width: CGFloat? = nil, height: CGFloat? = nil) {
@@ -143,7 +146,4 @@ extension UIView {
         }
     }
 }
-
-
-
 
