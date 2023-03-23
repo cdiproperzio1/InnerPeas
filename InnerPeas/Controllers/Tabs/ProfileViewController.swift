3857854f06dc97e4d021662af70667374cebe968
cdiproperzio1 <catherine.diproperzio@gmail.com>
2023-03-23 12:53:22 -0400

    uname changes

--- InnerPeas/Controllers/Tabs/ProfileViewController.swift
+++ InnerPeas/Controllers/Tabs/ProfileViewController.swift
@@ -9,23 +9,10 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-  //  var ref: DatabaseReference!
-//    ref = Database.database().reference()
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        title = "Profile"
-//        view.backgroundColor = .systemBackground
-//
-//        commentsRef.observe(.childAdded, with: { (snapshot) -> Void in
-//          self.comments.append(snapshot)
-//          print(snapshot)
-//          print(comments)
-//          self.tableView.insertRows(
-//            at: [IndexPath(row: self.comments.count - 1, section: self.kSectionComments)],
-//            with: UITableView.RowAnimation.automatic
-//          )
-//        })
-//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Profile"
+            }
     
 
     /*
